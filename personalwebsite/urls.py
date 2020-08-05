9adaa974e1b12349f89ee5a52bdc8f5410650108
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-08-05 20:48:50 +0530

    Using the navbar with include tag

--- personalwebsite/urls.py
+++ personalwebsite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages import views
+from pages.views import home_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name="Home")
+    path('', home_view, name="Home"),
+    path('about/', about_view, name="About")
+
     
 ]
